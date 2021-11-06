1077d1aae0d98fe503f1c774af705bc60c9f0d18
aeon32 <aeon32@mail.ru>
2021-11-06 22:23:33 +0300

    Simple test for button support. Related to #4

--- projects/blinky/main.c
+++ projects/blinky/main.c
@@ -7,7 +7,7 @@
 //total leds number
 #define ESTC_LEDS_NUMBER 4
 
-//pin numbers. Constants are defined in nRF52840 Dongle UserGuide
+//pin numbers for leds. Constants are defined in nRF52840 Dongle UserGuide
 static const uint8_t ESTC_LEDS_PINS[ESTC_LEDS_NUMBER] = 
    {
     NRF_GPIO_PIN_MAP(0,6),
@@ -16,7 +16,18 @@ static const uint8_t ESTC_LEDS_PINS[ESTC_LEDS_NUMBER] =
     NRF_GPIO_PIN_MAP(0,12)
    };
 
-static const int ESTC_LED_BLINK_TIMES[ESTC_LEDS_NUMBER] = {6, 5, 9, 9}; //equal to device id
+//pin numbers for button. Constants are defined in nRF52840 Dongle UserGuide
+static const uint8_t ESTC_BUTTON_PIN = NRF_GPIO_PIN_MAP(1,6);
+
+
+//we encode blinking sequence as array of led number (equal to (6, 5, 9, 9) - device id)
+static const int ESTC_BLINK_SEQUENCE[] = {
+    0, 0, 0, 0, 0, 0,
+    1, 1, 1, 1, 1,
+    2, 2, 2, 2, 2, 2, 2, 2, 2,
+    3, 3, 3, 3, 3, 3, 3, 3, 3 
+
+};
 static const int ESTC_LED_ON_TIME = 500;   //Time period of led switched on/off   
 
 void configure_gpio()
@@ -32,6 +43,9 @@ void configure_gpio()
        //switch off led by setting high level
        nrf_gpio_pin_write(pin_number, 1);
    };
+
+   nrf_gpio_cfg(ESTC_BUTTON_PIN, NRF_GPIO_PIN_DIR_INPUT, NRF_GPIO_PIN_INPUT_CONNECT,
+                 NRF_GPIO_PIN_PULLUP, NRF_GPIO_PIN_S0S1, NRF_GPIO_PIN_NOSENSE);   
 
 };
 
@@ -62,24 +76,31 @@ int main(void)
     /* Configure board. */
     configure_gpio();
 
-    int current_led = 0;
+    unsigned int sequence_step = 0;
+    
+    while (true)
+    {
+        uint32_t state = nrf_gpio_pin_read(ESTC_BUTTON_PIN );
+        nrf_gpio_pin_write( ESTC_LEDS_PINS[0], state);
+
+
+    };
+
+
+    const size_t SEQUENCE_SIZE = sizeof(ESTC_BLINK_SEQUENCE)/sizeof(ESTC_BLINK_SEQUENCE[0]);
     /* Toggle LEDs. */
     while (true)
     {
 
+        int current_led = ESTC_BLINK_SEQUENCE[sequence_step];
+        switch_led_on(current_led);
+        nrf_delay_ms(ESTC_LED_ON_TIME);
+        switch_led_off(current_led);
+        nrf_delay_ms(ESTC_LED_ON_TIME);
 
-        for (int i = 0; i < ESTC_LED_BLINK_TIMES[current_led]; i++)
-        {
-            
-            switch_led_on(current_led);
-            nrf_delay_ms(ESTC_LED_ON_TIME);
-            switch_led_off(current_led);
-            nrf_delay_ms(ESTC_LED_ON_TIME);
-        }
-
-        current_led++;
-        if (current_led == ESTC_LEDS_NUMBER)    
-            current_led = 0;
+        sequence_step++;
+        if (sequence_step == SEQUENCE_SIZE)    
+            sequence_step = 0;
         
     }
 }
